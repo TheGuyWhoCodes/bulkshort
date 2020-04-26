79b46ad77c3bce79b6c42d182984801858608b85
Christopher Ariagno <christopherariagno@gmail.com>
2020-04-25 23:30:36 -0500

    fix

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/bulkurl'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/bulkurl/index.html'));
 });
-// Start the app by listening on the default
-// Heroku port
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
